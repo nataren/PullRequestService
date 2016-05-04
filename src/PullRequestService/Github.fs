b709c8267d8ac8eac8767605602a184a47403bde
Cesar Lopez-Nataren <cesarn@mindtouch.com>
2016-05-04 13:55:19 -0700

    Include the correct name on the commit message

--- src/PullRequestService/Github.fs
+++ src/PullRequestService/Github.fs
@@ -272,6 +272,6 @@ type t(owner, token) =
         |> Seq.iter (fun (branch, date) ->
             logger.DebugFormat("targetBranch '{0}'\t sourceBranch '{1}'", date.ToSafeUniversalTime(), sourceBranch.ToSafeUniversalTime())
             if date.ToSafeUniversalTime() > sourceBranch.ToSafeUniversalTime() then      
-                this.MergeBranch prMetadata.Repo commit branch autoMergingMessage |> ignore else // TODO(cesarn): handle errors
+                this.MergeBranch prMetadata.Repo commit branch (autoMergingMessage + branch) |> ignore else // TODO(cesarn): handle errors
                 logger.DebugFormat("On repo '{0}', won't merge commit '{1}' to target branch '{2}' from '{3}' with message '{4}', ", prMetadata.Repo, commit, branch, sourceBranch.ToSafeUniversalTime(), autoMergingMessage + branch)
         )
